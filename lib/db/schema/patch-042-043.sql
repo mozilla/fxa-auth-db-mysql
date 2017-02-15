74586747531335a526cf6151690f755142f10b0a
vladikoff <vlad.filippov@gmail.com>
2017-02-15 09:42:12 -0500

    fix(prune): add index to unblock codes, update variable name

--- lib/db/schema/patch-042-043.sql
+++ lib/db/schema/patch-042-043.sql
@@ -1,14 +1,20 @@
-CREATE PROCEDURE `prune_2` (IN pruneTokensMaxAge BIGINT UNSIGNED)
+-- Add index to unblockCodes table
+CREATE INDEX unblockCodes_createdAt
+ON unblockCodes (createdAt)
+ALGORITHM = INPLACE LOCK = NONE;
+
+-- New prune procedure
+CREATE PROCEDURE `prune_2` (IN pruneTokensOlderThanDate BIGINT UNSIGNED)
 BEGIN
     -- try and obtain the prune lock
     SELECT @lockAcquired:=GET_LOCK('fxa-auth-server.prune-lock', 3);
 
     IF @lockAcquired THEN
 
-      DELETE FROM accountResetTokens WHERE createdAt < pruneTokensMaxAge ORDER BY createdAt LIMIT 10000;
-      DELETE FROM passwordForgotTokens WHERE createdAt < pruneTokensMaxAge ORDER BY createdAt LIMIT 10000;
-      DELETE FROM passwordChangeTokens WHERE createdAt < pruneTokensMaxAge ORDER BY createdAt LIMIT 10000;
-      DELETE FROM unblockCodes WHERE createdAt < pruneTokensMaxAge ORDER BY createdAt LIMIT 10000;
+      DELETE FROM accountResetTokens WHERE createdAt < pruneTokensOlderThanDate ORDER BY createdAt LIMIT 10000;
+      DELETE FROM passwordForgotTokens WHERE createdAt < pruneTokensOlderThanDate ORDER BY createdAt LIMIT 10000;
+      DELETE FROM passwordChangeTokens WHERE createdAt < pruneTokensOlderThanDate ORDER BY createdAt LIMIT 10000;
+      DELETE FROM unblockCodes WHERE createdAt < pruneTokensOlderThanDate ORDER BY createdAt LIMIT 10000;
 
       -- release the lock
       SELECT RELEASE_LOCK('fxa-auth-server.prune-lock');
