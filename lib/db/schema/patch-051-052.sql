ca40bc618b3753f65612056ecaf58b5d3b8ea980
Vijay Budhram <vbudhram@mozilla.com>
2017-06-20 23:01:50 -0400

    feat(email): Use select and union distinct

--- lib/db/schema/patch-051-052.sql
+++ lib/db/schema/patch-051-052.sql
@@ -2,7 +2,34 @@ CREATE PROCEDURE `accountEmails_3` (
     IN `inUid` BINARY(16)
 )
 BEGIN
-    SELECT * FROM emails WHERE uid = inUid ORDER BY isPrimary=true DESC;
+	(SELECT
+        a.normalizedEmail,
+        a.email,
+        a.uid,
+        a.emailCode,
+        a.emailVerified AS isVerified,
+        TRUE AS isPrimary,
+        a.createdAt
+    FROM
+        accounts a
+    WHERE
+        uid = inUid)
+
+    UNION DISTINCT
+
+    (SELECT
+        e.normalizedEmail,
+        e.email,
+        e.uid,
+        e.emailCode,
+        e.isVerified,
+        e.isPrimary,
+        e.createdAt
+    FROM
+        emails e
+    WHERE
+        uid = inUid)
+    ORDER BY isPrimary DESC;
 END;
 
 UPDATE dbMetadata SET value = '52' WHERE name = 'schema-patch-level';
