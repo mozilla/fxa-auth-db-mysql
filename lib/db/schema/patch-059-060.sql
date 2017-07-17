16d37d269a086f57b32ff57d21872815f5be77b5
Phil Booth <pmbooth@gmail.com>
2017-07-17 17:19:20 +0100

    fix(schema): call old sproc versions from session token sprocs

--- lib/db/schema/patch-059-060.sql
+++ lib/db/schema/patch-059-060.sql
@@ -12,15 +12,7 @@ CREATE PROCEDURE `createSessionToken_6` (
   IN `mustVerify` BOOLEAN
 )
 BEGIN
-  DECLARE EXIT HANDLER FOR SQLEXCEPTION
-  BEGIN
-    ROLLBACK;
-    RESIGNAL;
-  END;
-
-  START TRANSACTION;
-
-  INSERT INTO sessionTokens(
+  CALL createSessionToken_4(
     tokenId,
     tokenData,
     uid,
@@ -30,198 +22,44 @@ BEGIN
     uaOS,
     uaOSVersion,
     uaDeviceType,
-    lastAccessTime
-  )
-  VALUES(
-    tokenId,
-    tokenData,
-    uid,
-    createdAt,
-    uaBrowser,
-    uaBrowserVersion,
-    uaOS,
-    uaOSVersion,
-    uaDeviceType,
-    createdAt
+    tokenVerificationId,
+    mustVerify
   );
-
-  IF tokenVerificationId IS NOT NULL THEN
-    INSERT INTO unverifiedTokens(
-      tokenId,
-      tokenVerificationId,
-      uid,
-      mustVerify
-    )
-    VALUES(
-      tokenId,
-      tokenVerificationId,
-      uid,
-      mustVerify
-    );
-  END IF;
-
-  COMMIT;
 END;
 
 CREATE PROCEDURE `sessionToken_6` (
   IN `tokenIdArg` BINARY(32)
 )
 BEGIN
-  SELECT
-    t.tokenData,
-    t.uid,
-    t.createdAt,
-    t.uaBrowser,
-    t.uaBrowserVersion,
-    t.uaOS,
-    t.uaOSVersion,
-    t.uaDeviceType,
-    t.lastAccessTime,
-    e.isVerified AS emailVerified,
-    e.email,
-    e.emailCode,
-    a.verifierSetAt,
-    a.locale,
-    a.createdAt AS accountCreatedAt
-  FROM sessionTokens AS t
-  LEFT JOIN accounts AS a
-    ON t.uid = a.uid
-  LEFT JOIN emails AS e
-    ON t.uid = e.uid
-    AND e.isPrimary = true
-  WHERE t.tokenId = tokenIdArg;
+  CALL sessionToken_4(tokenIdArg);
 END;
 
 CREATE PROCEDURE `sessionTokenWithVerificationStatus_5` (
   IN `tokenIdArg` BINARY(32)
 )
 BEGIN
-  SELECT
-    t.tokenData,
-    t.uid,
-    t.createdAt,
-    t.uaBrowser,
-    t.uaBrowserVersion,
-    t.uaOS,
-    t.uaOSVersion,
-    t.uaDeviceType,
-    t.lastAccessTime,
-    e.isVerified AS emailVerified,
-    e.email,
-    e.emailCode,
-    a.verifierSetAt,
-    a.locale,
-    a.createdAt AS accountCreatedAt,
-    ut.tokenVerificationId,
-    ut.mustVerify
-  FROM sessionTokens AS t
-  LEFT JOIN accounts AS a
-    ON t.uid = a.uid
-  LEFT JOIN emails AS e
-    ON t.uid = e.uid
-    AND e.isPrimary = true
-  LEFT JOIN unverifiedTokens AS ut
-    ON t.tokenId = ut.tokenId
-  WHERE t.tokenId = tokenIdArg;
+  CALL sessionTokenWithVerificationStatus_3(tokenIdArg);
 END;
 
 CREATE PROCEDURE `sessionWithDevice_7` (
   IN `tokenIdArg` BINARY(32)
 )
 BEGIN
-  SELECT
-    t.tokenData,
-    t.uid,
-    t.createdAt,
-    t.uaBrowser,
-    t.uaBrowserVersion,
-    t.uaOS,
-    t.uaOSVersion,
-    t.uaDeviceType,
-    t.lastAccessTime,
-    e.isVerified AS emailVerified,
-    e.email,
-    e.emailCode,
-    a.verifierSetAt,
-    a.locale,
-    a.createdAt AS accountCreatedAt,
-    d.id AS deviceId,
-    d.name AS deviceName,
-    d.type AS deviceType,
-    d.createdAt AS deviceCreatedAt,
-    d.callbackURL AS deviceCallbackURL,
-    d.callbackPublicKey AS deviceCallbackPublicKey,
-    d.callbackAuthKey AS deviceCallbackAuthKey,
-    ut.tokenVerificationId,
-    ut.mustVerify
-  FROM sessionTokens AS t
-  LEFT JOIN accounts AS a
-    ON t.uid = a.uid
-  LEFT JOIN emails AS e
-    ON t.uid = e.uid
-    AND e.isPrimary = true
-  LEFT JOIN devices AS d
-    ON (t.tokenId = d.sessionTokenId AND t.uid = d.uid)
-  LEFT JOIN unverifiedTokens AS ut
-    ON t.tokenId = ut.tokenId
-  WHERE t.tokenId = tokenIdArg;
+  CALL sessionWithDevice_5(tokenIdArg);
 END;
 
 CREATE PROCEDURE `sessions_4` (
   IN `uidArg` BINARY(16)
 )
 BEGIN
-  SELECT
-    t.tokenId,
-    t.uid,
-    t.createdAt,
-    t.uaBrowser,
-    t.uaBrowserVersion,
-    t.uaOS,
-    t.uaOSVersion,
-    t.uaDeviceType,
-    t.lastAccessTime,
-    d.id AS deviceId,
-    d.name AS deviceName,
-    d.type AS deviceType,
-    d.createdAt AS deviceCreatedAt,
-    d.callbackURL AS deviceCallbackURL,
-    d.callbackPublicKey AS deviceCallbackPublicKey,
-    d.callbackAuthKey AS deviceCallbackAuthKey
-  FROM sessionTokens AS t
-  LEFT JOIN devices AS d
-    ON (t.tokenId = d.sessionTokenId AND t.uid = d.uid)
-  WHERE t.uid = uidArg;
+  CALL sessions_2(uidArg);
 END;
 
 CREATE PROCEDURE `accountDevices_9` (
   IN `uidArg` BINARY(16)
 )
 BEGIN
-  SELECT
-    d.uid,
-    d.id,
-    d.sessionTokenId,
-    d.name,
-    d.type,
-    d.createdAt,
-    d.callbackURL,
-    d.callbackPublicKey,
-    d.callbackAuthKey,
-    s.uaBrowser,
-    s.uaBrowserVersion,
-    s.uaOS,
-    s.uaOSVersion,
-    s.uaDeviceType,
-    s.lastAccessTime,
-    e.email
-  FROM devices AS d
-  INNER JOIN sessionTokens AS s
-    ON d.sessionTokenId = s.tokenId
-  INNER JOIN emails AS e
-    ON d.uid = e.uid
-    AND e.isPrimary = true
-  WHERE d.uid = uidArg;
+  CALL accountDevices_7(uidArg);
 END;
 
 ALTER TABLE `sessionTokens`
