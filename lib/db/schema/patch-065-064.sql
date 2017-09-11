36a9fa92980df4d6a1e540c829a2ad3a06012e53
vladikoff <vlad.filippov@gmail.com>
2017-09-11 18:58:58 -0400

    fix(mysql): add drop proc

--- lib/db/schema/patch-065-064.sql
+++ lib/db/schema/patch-065-064.sql
@@ -1,4 +1,7 @@
--- -- TODO: Implement your *commented-out* reverse migration here
+-- DROP PROCEDURE `accountDevices_12`;
+-- DROP PROCEDURE `updateDevice_5`;
+-- DROP PROCEDURE `deviceFromTokenVerificationId_3`;
+-- DROP PROCEDURE `sessions_7`;
+-- DROP PROCEDURE `sessionWithDevice_10`;
 
 -- UPDATE dbMetadata SET value = '64' WHERE name = 'schema-patch-level';
-
